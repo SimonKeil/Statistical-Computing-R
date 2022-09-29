fc867e821ccd256dd29ccea144f6c8472dd4625c
Simon Keil <113024431+SimonKeil@users.noreply.github.com>
2022-09-29 19:51:16 +0200

    MCMC continued

--- markov-chain-monte-carlo.R
+++ markov-chain-monte-carlo.R
@@ -81,3 +81,59 @@ hist(X[1000:m])
 mean(X[1000:m])
 
 
+
+
+## Generate Rayleigh(4) distribution using MCMC
+# target distribution
+f <- function(x) {
+  if(x < 0)
+    return(0)
+  return(x/16*exp(-x^2/32))
+}
+
+N <- 10000
+burn <- N/5
+# the chain
+X <- numeric(N)
+X[1] = 1
+U <- runif(N)
+acc <- 0 # record acceptance
+for(t in 2:N) {
+  # proposal density is Gamma(X, 1)
+  Y <- rgamma(1,X[t-1],1)
+  r <- f(Y)*dgamma(X[t-1], Y, 1)/(f(X[t-1])*dgamma(Y, X[t-1], 1))
+  if(U[t] <= r) {
+    X[t] = Y
+    acc = acc + 1
+  } else {
+    X[t] = X[t-1]
+  }
+}
+acc/N #acceptance rate
+plot(X, type = "l")
+hist(X[burn+1:N])
+
+
+
+
+## Gibbs sampler for bivariate normal distribution
+# target means and sd
+mu1 <- 2
+mu2 <- 3
+s1 <- 1
+s2 <- 1
+rho <- .5
+# the chain
+N <- 10000
+X <- matrix(0, N, 2)
+X[1, ] =c(mu1, mu2)
+for(i in 2:N) {
+  X[i,1] = rnorm(1, mu1 + rho*s1/s2*(X[i-1,2] - mu2), s1*sqrt(1-rho^2))
+  X[i,2] = rnorm(1, mu2 + rho*s2/s1*(X[i,1] - mu1), s2*sqrt(1-rho^2))
+}
+# check means and variance
+colMeans(X)
+cbind(var(X[,1]),var(X[,2]))
+# check plot (rotated ellipse with center (2,3))
+plot(X)
+
